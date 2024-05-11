7f7802529c3ecf0bf20f4e39cad6620f50e7cc0b
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-05-11 12:30:34 -0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <h1 class="bg-primary">asa</h1>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+};
+
